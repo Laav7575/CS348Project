588720f666d5a266925a14b127a7722143ec9a03
Angela Huang <93748376+candycane124@users.noreply.github.com>
2025-05-21 21:53:33 -0400

    Setup basic application

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <div className="text-center grow-1 text-2xl font-bold">Hello World</div>
+    </div>
+  );
+}
+// x/2+5+2n
