558bf76f6d8579c686da2d353ff609d1b80df90d
Angela Huang <93748376+candycane124@users.noreply.github.com>
2025-06-11 19:58:43 -0400

    Add blank pages for home & search

---
+++ src/app/search/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../../components/NavBar";
+
+export default function Home() {
+  return (
+    <div className="bg-gray-900 h-screen w-screen">
+      <NavBar />
+    </div>
+  );
+};
+
