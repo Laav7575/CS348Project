e0b34ef230448491a7101d09ff839b9fe5359a3a
Laavanya Thiagalingam <laavanya.thiaga@gmail.com>
2025-07-04 01:15:41 -0400

    changed table name

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -8,8 +8,22 @@ DROP TABLE IF EXISTS Reviews;
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Cars;
 
+
+CREATE TABLE IF NOT EXISTS Cars (
+    cID INT AUTO_INCREMENT NOT NULL,
+    make VARCHAR(30) NOT NULL,
+    model VARCHAR(30) NOT NULL,
+    year INT,
+    engineSize FLOAT,
+    horsePower FLOAT,
+    torque FLOAT,
+    acceleration FLOAT,
+    price FLOAT,
+    PRIMARY KEY(cID)
+);
+
 -- create tables
-CREATE TABLE Cars (
+CREATE TABLE testCars (
    cID                     INT AUTO_INCREMENT not NULL,
    make                    VARCHAR(50) not NULL,
    model                   VARCHAR(50) not NULL,
@@ -61,31 +75,31 @@ CREATE TABLE Saves(
 );
 
 -- insert test data
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Porsche', '911', 2022, FALSE, 3.0, 379.0, 331.0, 4.0, 101200.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Lamborghini', 'Huracan', 2021, FALSE, 5.2, 630.0, 443.0, 2.8, 274390.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Ferrari', '488 GTB', 2022, FALSE, 3.9, 661.0, 561.0, 3.0, 333750.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Audi', 'R8', 2022, FALSE, 5.2, 562.0, 406.0, 3.2, 142700.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('McLaren', '720S', 2021, FALSE, 4.0, 710.0, 568.0, 2.7, 298000.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('BMW', 'M8', 2022, FALSE, 4.4, 617.0, 553.0, 3.1, 130000.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Mercedes-Benz', 'AMG GT', 2021, FALSE, 4.0, 523.0, 494.0, 3.8, 118500.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Chevrolet', 'Corvette', 2021, FALSE, 6.2, 490.0, 465.0, 2.8, 59900.0);
 
-INSERT INTO Cars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
+INSERT INTO testCars (make, model, year, isElectric, engineSize, horsePower, torque, acceleration, price)
 VALUES ('Ford', 'Mustang Shelby GT500', 2022, FALSE, 5.2, 760.0, 625.0, 3.5, 81000.0);
 
 INSERT INTO Users (username, email, userPassword, isAdmin, isDeleted) VALUES
@@ -119,19 +133,6 @@ INSERT INTO Saves (fID, cID) VALUES
 (2, 2),
 (3, 3),
 (4, 4);
-
-CREATE TABLE IF NOT EXISTS fullCars (
-    cID INT AUTO_INCREMENT NOT NULL,
-    make VARCHAR(30) NOT NULL,
-    model VARCHAR(30) NOT NULL,
-    year INT,
-    engineSize FLOAT,
-    horsePower FLOAT,
-    torque FLOAT,
-    acceleration FLOAT,
-    price FLOAT,
-    PRIMARY KEY(cID)
-);
 
 -- Load data from CSV
 -- IMPORTANT: The path '/docker-entrypoint-initdb.d/your_dataset.csv' is relative to the MySQL container's filesystem.
